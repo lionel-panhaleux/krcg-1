72bfea6712bc523bdbc21dd69dc4ad0a2858138b
Spigushe <martin.cuchet@gmail.com>
2020-05-18 20:57:01 +0200

    new page
    Renaming template function to template_index
    
    adding a new route : /strategy/
    adding a new template for strategy
    
    moving "hello world" template to "/test"

--- run.py
+++ run.py
@@ -3,10 +3,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def template_test():
+def template_index():
     return render_template('index.html')
 
 @app.route("/strategy/")
+def template_strategy():
+    return render_template('strategy.html')
+
+@app.route("/test")
 def template_strategy():
     return render_template('template.html', my_string="Noooo!", my_list=[2,5,6,7])
 
