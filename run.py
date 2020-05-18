02ac2a5ce4444c32afe7c07fc7db6cc2f25f7c55
Spigushe <martin.cuchet@gmail.com>
2020-05-18 20:32:26 +0200

    Initial commit

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def template_test():
+    return render_template('index.html')
+
+@app.route("/strategy/")
+def template_strategy():
+    return render_template('template.html', my_string="Noooo!", my_list=[2,5,6,7])
+
+if __name__ == '__main__':
+    app.run(debug=True)
